84623b10459b27cb9f6fa3e14737c8af5848415e
Zahra Bahmanpour <z.bmnpr@gmail.com>
2024-06-05 10:55:41 +0330

    add search input to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Header() {
+  const [searchParams, setSearchParams] = useSearchParams();
   return (
     <div>
       <Link to="/">NatureGram</Link>
+      <input
+        type="text"
+        placeholder="search here..."
+        onChange={(e) => setSearchParams({ title: e.target.value })}
+      />
       <Link to="/createNew">Create New</Link>
       <Link to="/about">About</Link>
     </div>
